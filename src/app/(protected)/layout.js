1d62048e8238ec2b4734a33985d232dfe7c483d1
Abhiraj-Sagar <abhiraj.sagar@soniconsultancy.in>
2026-02-15 16:35:40 +0530

    Dynamic Layouting FIx

--- src/app/(protected)/layout.js
+++ src/app/(protected)/layout.js
@@ -1,10 +1,12 @@
 import { getCurrentUser } from "@/services/getCurrentUser";
+import { redirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
 
 export default async function ProtectedLayout({children})
 {
     const user = await getCurrentUser();
-    if(!user) return redirect("/auth");
-    console.log(user);
+    if(!user) redirect("/auth");
 
     return children;
 }
